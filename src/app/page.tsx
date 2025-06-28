8aa38e64f8ca491d3a3d203d691600c47dc6e385
MirosBL45 <jovicmiroslav88@gmail.com>
2025-06-29 01:02:26 +0200

    adding user to convex DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { SignOutButton } from "@clerk/nextjs";
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
     <div>HomePage
       <SignOutButton />
+      <SignInButton />
     </div>
   )
 }
