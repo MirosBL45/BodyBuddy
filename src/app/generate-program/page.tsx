e01604179fd075a1f937aaeffbf2526941476fef
MirosBL45 <jovicmiroslav88@gmail.com>
2025-07-19 21:16:29 +0200

    started generate program

--- src/app/generate-program/page.tsx
+++ src/app/generate-program/page.tsx
@@ -1,4 +1,21 @@
+"use client";
+
+import { useState, useRef } from "react";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
 export default function GenerateProgramPage() {
+  const [callActive, setCallActive] = useState(false);
+  const [connecting, setConnecting] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [messages, setMessages] = useState<any[]>([]);
+  const [callEnded, setCallEnded] = useState(false);
+
+  const { user } = useUser();
+  const router = useRouter();
+
+  const messageContainerRef = useRef<HTMLDivElement>(null);
+
   return (
     <div>GenerateProgramPage</div>
   )
