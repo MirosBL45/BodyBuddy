e98d9cd0ad6d3efa3a6305afb16ab6a3f0c138eb
MirosBL45 <jovicmiroslav88@gmail.com>
2025-07-30 22:02:08 +0200

    fatching plans

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,34 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+import { useQuery } from 'convex/react';
+import { api } from '../../../convex/_generated/api';
+import { useState } from 'react';
+import ProfileHeader from '@/components/ProfileHeader';
+import NoFitnessPlan from '@/components/NoFitnessPlan';
+
 export default function ProfilePage() {
+  const { user } = useUser();
+  const userId = user?.id as string;
+
+  const allPlans = useQuery(api.plans.getUserPlans, { userId });
+  const [selectedPlanId, setSelectedPlanId] = useState<null | string>(null);
+
+  const activePlan = allPlans?.find((plan) => plan.isActive);
+
+  const currentPlan = selectedPlanId
+    ? allPlans?.find((plan) => plan._id === selectedPlanId)
+    : activePlan;
+
   return (
-    <div>ProfilePage</div>
-  )
+    <section className="relative z-10 pt-12 pb-32 flex-grow container mx-auto px-4">
+      <ProfileHeader user={user} />
+
+      {allPlans && allPlans?.length > 0 ? (
+        <div>you got he plan</div>
+      ) : (
+        <NoFitnessPlan />
+      )}
+    </section>
+  );
 }
