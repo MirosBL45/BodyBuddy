48183e722526052379277540ffc3b4b1ea74d187
MirosBL45 <jovicmiroslav88@gmail.com>
2025-09-30 15:32:38 +0200

    contact progress

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,7 @@
+import WorkInProgress from "@/components/WorkInProgress";
+
 export default function ContactPage() {
   return (
-    <div>This is ContactPage</div>
+    <WorkInProgress title="Contact page coming soon" message="We're building this page — stay tuned!" />
   )
 }
